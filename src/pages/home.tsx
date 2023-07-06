0defbd7b2ab3dfed0b2b170616bdefb7032fedb0
vtsuellen <vtsuellen@hotmail.com>
2023-07-05 21:53:47 -0300

    feat:Liste as categorias de produtos disponíveis

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../services/api';
 
 function Home() {
-  const [list, setList] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState<{ id: string, name: string }[]>([]);
+
+  const getCategories = async () => {
+    const categorie = await api.getCategories();
+    setCategories(categorie);
+  };
+
+  // vai chamar a funcão da API e realizar uma vez
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <>
-      {/* Campo de busca */}
-      <input type="text" placeholder="Buscar" />
+      <div>
+        {/* Campo de busca */}
+        <input type="text" placeholder="Buscar" />
+
+        {/* Botão do carrinho de compras */}
+        <Link to="/carrinho" data-testid="shopping-cart-button">
+          {/* img não esta rendezirando */}
+          <img src="./src/img/carrinho.png" alt="Carrinho de Compras" />
+        </Link>
+
+      </div>
 
       {/* Quando a lista estiver vazia, a página deverá mostrar a mensagem "Digite algum termo de pesquisa ou escolha uma categoria." */}
       <div>
-        { list.length === 0
-          ? (
-            <h1 data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </h1>) : <p>Aqui vai a listagem de produtos</p>}
+        {products.length === 0 ? (
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        ) : (
+          <p>Aqui vai a listagem de produtos</p>
+        )}
       </div>
 
-      {/* Botão do carrinho de compras */}
-      <Link to="/carrinho" data-testid="shopping-cart-button">
-        <img src="./src/img/carrinho.png" alt="Carrinho de Compras" />
-      </Link>
+      <aside>
+        {categories.length > 0 && categories
+          .map((categorie) => (
+            <button
+              data-testid="category"
+              key={ categorie.id }
+            >
+              {categorie.name}
+            </button>
+          ))}
+      </aside>
     </>
   );
 }
