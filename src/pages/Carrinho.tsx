1c9ccdc2b9602a4e3f8e3d1f3852ad3d0d436557
kaique-cadimiel-dev <kaiqueamasio@gmail.com>
2023-07-07 14:24:10 -0300

    feat: Adiciona produtos ao carrinho a partir da tela de listagem de produtos

--- src/pages/Carrinho.tsx
+++ src/pages/Carrinho.tsx
@@ -1,15 +1,43 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductsProps } from './home';
 
 function Carrinho() {
+  const [itensCart, setItemsCart] = useState<ProductsProps[]>([]);
   const carrinhoVazio = true; // Defina essa variável com base no estado do carrinho
+  /* pega lista de carts do localStorage */
+  const addCart = () => {
+    const resultado = localStorage.getItem('dataCart');
+    if (resultado) {
+      const getItensCart = JSON.parse(resultado);
+      setItemsCart(getItensCart);
+      console.log(getItensCart);
+    }
+  };
+
+  useEffect(() => {
+    addCart();
+  }, []);
 
   return (
     <div>
-      {carrinhoVazio ? (
+      {carrinhoVazio && itensCart.length === 0 ? (
         <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
       ) : (
         <div>
-          {/* Exibir conteúdo do carrinho */}
+          {
+            itensCart.map((element) => (
+              <div key={ element.id }>
+                <span data-testid="shopping-cart-product-name">{element.title}</span>
+                <img src={ element.thumbnail } alt={ `${element.title} thumbnail` } />
+                <span>
+                  {`R$ ${element.price}`}
+
+                </span>
+                <span data-testid="shopping-cart-product-quantity">1</span>
+              </div>
+            ))
+          }
         </div>
       )}
 
